014ba9fead83c6604221eca8aa54f328419b57df
Hong Son Ngo <toilahongson@gmail.com>
2025-11-16 12:38:28 +0100

    Adding diffview.nvim plugin

--- init.lua
+++ init.lua
@@ -14,7 +14,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -157,6 +157,7 @@ require("lazy").setup({
     require "plugins.treesitter",
     require "plugins.neo-tree",
     require "plugins.lsp-config",
+    require "plugins.diffview",
   },
   checker = { enabled = true },
 })
