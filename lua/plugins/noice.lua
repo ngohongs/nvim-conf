085edd6fe6ad5fb28940e8db2cdbdadeb8c01f1a
Hong Son Ngo <toilahongson@gmail.com>
2025-11-17 20:22:37 +0100

    Removing redundant settings

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -10,9 +10,6 @@ return {
                 ["vim.lsp.util.stylize_markdown"] = true,
                 ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
             },
-            hover = {
-                enabled = false,
-            },
             signature = {
                 enabled = false,
             },
@@ -24,13 +21,6 @@ return {
             long_message_to_split = true, -- long messages will be sent to a split
             inc_rename = false,           -- enables an input dialog for inc-rename.nvim
             lsp_doc_border = true,       -- add a border to hover docs and signature help
-        },
-        views = {
-            hover = {
-                border = {
-                    style = "rounded",
-                },
-            },
         },
     },
     dependencies = {
