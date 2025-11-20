079b6595a96bafd09b76ab5430299d99b3cb98f9
Hong Son Ngo <toilahongson@gmail.com>
2025-11-20 21:08:24 +0100

    Allowing telescope find_files to show hidden and git ignored files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -20,7 +20,8 @@ return { -- Fuzzy Finder (files, lsp, etc)
         {
             'nvim-telescope/telescope-fzf-native.nvim',
 
-            build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install',
+            build =
+            'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install',
 
             cond = function()
                 return vim.fn.executable 'make' ~= 1
@@ -123,11 +124,19 @@ return { -- Fuzzy Finder (files, lsp, etc)
         pcall(require('telescope').load_extension, 'fzf')
         pcall(require('telescope').load_extension, 'ui-select')
 
+
         -- See `:help telescope.builtin`
         local builtin = require 'telescope.builtin'
         vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
         vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-        vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+        vim.keymap.set('n', '<leader>sf',
+            function()
+                require('telescope.builtin').find_files {
+                    hidden = true,
+                    no_ignore = true,
+                }
+            end
+            , { desc = '[S]earch [F]iles' })
         vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
         vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
         vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
