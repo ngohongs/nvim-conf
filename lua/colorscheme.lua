3b31d114f11422a3e218fd4e9c656a414e00f439
Hong Son Ngo <toilahongson@gmail.com>
2025-11-26 20:36:01 +0100

    Enable transparent theme

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -3,7 +3,9 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-        require("tokyonight").setup({})
+        require("tokyonight").setup({
+            transparent = true,
+        })
 
         vim.cmd [[colorscheme tokyonight]]
     end,
